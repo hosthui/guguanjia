95fbd719baf03d0ddbf3980eb6177b62c1b20855
Stephen <467198055@qq.com>
2020-11-03 21:04:56 +0800

    updata10

--- src/main/java/com/lyh/guguanjia/controller/MainController.java
+++ src/main/java/com/lyh/guguanjia/controller/MainController.java
@@ -1,12 +1,24 @@
 package com.lyh.guguanjia.controller;
 
 
+import com.baomidou.kaptcha.Kaptcha;
+import com.lyh.guguanjia.entity.Result;
+import com.lyh.guguanjia.entity.SysUser;
+import com.lyh.guguanjia.util.EncryptUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @RequestMapping("main")
 @Controller
 public class MainController {
+
+	@Autowired
+	private Kaptcha kaptcha;
+
 
 	@RequestMapping("sidebar")
 	public String sidebar(){
@@ -15,5 +27,23 @@ public class MainController {
 	@RequestMapping("navbar")
 	public String navbar(){
 		return "/common/navbar.html";
+	}
+
+	/**
+	 * 用户登录
+	 *
+	 */
+	@RequestMapping("login")
+	@ResponseBody
+	public Result loginIn(String username, String password, String code,
+	                      HttpSession session){
+		String message=null;
+		if ( kaptcha.validate(code) ){
+			SysUser loginUser = new SysUser();
+			loginUser.setUsername(username);
+			loginUser.setPassword(EncryptUtils.MD5_HEX(EncryptUtils.MD5_HEX(password)+username));
+		}
+
+		return new Result();
 	}
 }
